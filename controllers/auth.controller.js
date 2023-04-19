af185948737bc368128cf210899cdcf5b3c9f450
SafarovHusan200 <safarovhusan2000@gmail.com>
2023-04-20 01:34:05 +0500

    update Profile and Password

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -56,3 +56,64 @@ exports.login = asyncHandler(async (req, res, next) => {
     token,
   });
 });
+
+// @descr   Get profile User
+// @Route   GET /api/v1/auth/profile
+// @Access  Private
+exports.getProfile = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user._id);
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
+// @descr   Update profile User
+// @Route   PUT /api/v1/auth/update
+// @Access  Private
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user._id);
+
+  const fieldsToUpdate = {
+    name: req.body.name || user.name,
+    email: req.body.email || user.email,
+  };
+
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user.id,
+    fieldsToUpdate,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  res.status(200).json({
+    success: true,
+    data: updatedUser,
+  });
+});
+
+// @descr   Update password
+// @Route   PUT /api/v1/auth/updatepassword
+// @Access  Private
+exports.updatePassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user._id);
+
+  // Check current password
+  if (!(await user.matchPassword(req.body.currentPassword))) {
+    return next(new ErrorResponse("Old password is incorrect", 400));
+  }
+
+  user.password = req.body.newPassword;
+  await user.save();
+
+  const token = user.generateJwtToken();
+
+  res.status(200).json({
+    success: true,
+    data: user,
+    token,
+  });
+});
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,8 +1,18 @@
 const { Router } = require("express");
-const { register, login } = require("../controllers/auth.controller");
+const {
+  register,
+  login,
+  getProfile,
+  updateDetails,
+  updatePassword,
+} = require("../controllers/auth.controller");
 const router = Router();
+const { protected } = require("../middlewares/auth");
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/profile", protected, getProfile);
+router.put("/update", protected, updateDetails);
+router.put("/updatepassword", protected, updatePassword);
 
 module.exports = router;
